868b800d50292b80cf4fd89aac4d384c737e57a6
Lindsay Dennis <lindsay.c.dennis@gmail.com>
2018-03-20 15:32:22 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -34,7 +34,7 @@ class App < Sinatra::Base
     @num2 = params[:number2].to_i 
     @verb = params[:operation]
     if @verb == 'add'
-      @num1 + @num2 
+      "#{(@num1 + @num2)}" 
     elsif @verb == 'subtract'
       @num1 - @num2 
     elsif @verb == 'multiply'
